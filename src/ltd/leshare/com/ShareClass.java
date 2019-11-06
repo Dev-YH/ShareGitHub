ee97c0165b95736e91ff5301c480a71933625b6e
14113 <14113@192.168.202.1>
2019-11-06 18:55:03 +0800

    studey git V2.0

--- src/ltd/leshare/com/ShareClass.java
+++ src/ltd/leshare/com/ShareClass.java
@@ -3,5 +3,8 @@ package ltd.leshare.com;
 public class ShareClass {
 	public static void main(String[] args) {
 		System.out.println("Study Git ing...");
+		for (int i = 0; i <= 1000000; i++) {
+			System.out.println("day day up");
+		}
 	}
 }
